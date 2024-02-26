4f69869017a9594a1f3c0c1d440d9df110d6cabf
Yukkitsune <akito.merkulov@ya.ru>
2024-02-26 11:31:05 +0300

    added comments

--- examples/hello_world.cpp
+++ examples/hello_world.cpp
@@ -2,9 +2,9 @@
 #include <string>
 int main(int argc, char** argv)
 {
-	std::string name;
+	std::string name; // name variable
 	std::cout<<"Enter your name:";
-	std::cin >> name;
+	std::cin >> name; // input name
 	std::cout<<"Hello world from "<<name<<std::endl;
 	return 0;
 }
