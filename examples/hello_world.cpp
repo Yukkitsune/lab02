17812249b61b9193fde86de0a7cb729ee43b4689
Yukkitsune <akito.merkulov@ya.ru>
2024-02-26 11:17:44 +0300

    added name feature

--- examples/hello_world.cpp
+++ examples/hello_world.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <string>
 using namespace std;
-int main(int argc, char** argv){
-    cout<<"Hello world"<<endl;
-    return 0;
+int main(int argc, char** argv)
+{
+	string name;
+	cout<<"Enter your name:";
+	cin >> name;
+	cout<<"Hello world from "<<name<<endl;
+	return 0;
 }
