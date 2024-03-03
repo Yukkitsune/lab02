318779a207426045bac8732c6f6ed17ca0770700
Yukkitsune <akito.merkulov@ya.ru>
2024-03-03 21:48:35 +0300

    Changed code style to GNU

--- examples/hello_world.cpp
+++ examples/hello_world.cpp
@@ -1,8 +1,8 @@
 #include <iostream> // in and out stream
 #include <string> // string library
-int main(int argc, char** argv)
+int main(int argc, char** argv)  
 {
-	std::string name; // name variable
+	std::string name; // variable name
 	std::cout<<"Enter your name:";
 	std::cin >> name; // input name
 	std::cout<<"Hello world from "<<name<<std::endl;
